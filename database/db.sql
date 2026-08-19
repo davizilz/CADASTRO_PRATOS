7960152e59ee078c033f178d5152b03ab1ca1956
davi_zilz <davi_zilz@estudante.sesisenai.org.br>
2026-08-19 09:47:16 -0300

    up - atualização a tabela pratos do banco

--- database/db.sql
+++ database/db.sql
@@ -10,7 +10,7 @@ CREATE TABLE usuarios (
 CREATE TABLE pratos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(200) NOT NULL,
-    descricao TEXT,
+    descricao VARCHAR(200) NOT NULL,
     preco DECIMAL(10,2) NOT NULL,
     categoria VARCHAR(100) NOT NULL,
     usuario_id INT NOT NULL,
